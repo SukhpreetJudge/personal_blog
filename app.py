6861809eeb437a77f13ca73364073b95d5b1026f
SukhpreetJudge <sukhpreetjudge@gmail.com>
2020-01-30 11:52:31 -0800

    Setting up skeleton

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+	{
+		'author' : 'Sonny',
+		'post_title' : 'How to master coding',
+		'post_body' : 'Today I will teach you',
+		'date_posted' : 'Jan 30th 2020'
+	},
+	{
+		'author' : 'Pajeet',
+		'post_title' : 'Mastering Cleanliness',
+		'post_body' : 'Paper is for noobs',
+		'date_posted' : 'Jan 31st 2020'
+	}
+]
+
+
+@app.route("/")
+@app.route("/home")
+def homepage():
+	return render_template('home.html', posts = posts, title = 'Sonny\'s')
+
+
+@app.route("/about")
+def aboutPage():
+	return render_template('about.html', title = 'H')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
